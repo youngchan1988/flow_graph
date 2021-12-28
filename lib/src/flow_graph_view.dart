87aecfd5344108f1ee4de2085920dabfdbd49786
YoungChan <zhan.yanyang@xinheyun.com>
2021-12-28 09:41:39 +0800

    flow_graph_view add onSelectChanged

--- lib/src/flow_graph_view.dart
+++ lib/src/flow_graph_view.dart
@@ -1,36 +1,72 @@
 import 'package:flutter/material.dart';
 
+import 'focus.dart';
 import 'graph.dart';
 import 'graph_view.dart';
 
-class FlowGraphView extends StatelessWidget {
+class FlowGraphView<T> extends StatefulWidget {
   const FlowGraphView(
       {Key? key,
       required this.root,
       this.direction = Axis.horizontal,
       this.centerLayout = false,
-      required this.builder})
+      this.enabled = true,
+      required this.builder,
+      this.onSelectChanged})
       : super(key: key);
 
-  final GraphNode root;
+  final GraphNode<T> root;
   final Axis direction;
   final bool centerLayout;
-  final NodeWidgetBuilder builder;
+  final bool enabled;
+  final NodeWidgetBuilder<T> builder;
+  final OnSelectChanged<T>? onSelectChanged;
+
+  @override
+  _FlowGraphViewState<T> createState() => _FlowGraphViewState<T>();
+}
+
+class _FlowGraphViewState<T> extends State<FlowGraphView<T>> {
+  final GraphFocusManager _focusManager = GraphFocusManager();
 
   @override
   Widget build(BuildContext context) {
-    var graph = Graph(
-        nodes: _linearNodes(context, root),
-        direction: direction,
-        centerLayout: centerLayout);
-
     return Padding(
-        padding: const EdgeInsets.all(16), child: GraphView(graph: graph));
+      padding: const EdgeInsets.all(16),
+      child: GraphFocus(
+        manager: _focusManager,
+        child: Builder(
+          builder: (context) {
+            var graph = Graph(
+                nodes: _linearNodes(context, widget.root),
+                direction: widget.direction,
+                centerLayout: widget.centerLayout);
+
+            return GestureDetector(
+              onTap: () {
+                GraphFocus.of(context).clearFocus();
+                widget.onSelectChanged?.call(null);
+              },
+              child: GraphView<T>(graph: graph),
+            );
+          },
+        ),
+      ),
+    );
   }
 
-  //bfs
-  List<GraphNode> _linearNodes(BuildContext context, GraphNode root) {
-    root.initialElement(child: builder(context, root));
+//bfs
+  List<GraphNode> _linearNodes(BuildContext context, GraphNode<T> root) {
+    root.initialElement(
+        child: widget.enabled
+            ? _NodeWidget(
+                child: widget.builder(context, root),
+                node: root,
+                onFocus: () {
+                  widget.onSelectChanged?.call(root);
+                },
+              )
+            : widget.builder(context, root));
     var walked = <GraphNode>[root];
     var visited = <GraphNode>[root];
 
@@ -40,7 +76,16 @@ class FlowGraphView extends StatelessWidget {
         for (var i = 0; i < currentNode.nextList.length; i++) {
           var node = currentNode.nextList[i];
           if (!visited.contains(node)) {
-            node.initialElement(child: builder(context, node));
+            node.initialElement(
+                child: widget.enabled
+                    ? _NodeWidget(
+                        child: widget.builder(context, node as GraphNode<T>),
+                        node: node,
+                        onFocus: () {
+                          widget.onSelectChanged?.call(node);
+                        },
+                      )
+                    : widget.builder(context, node as GraphNode<T>));
             walked.add(node);
             visited.add(node);
           }
@@ -48,5 +93,80 @@ class FlowGraphView extends StatelessWidget {
       }
     }
     return visited;
+  }
+}
+
+class _NodeWidget extends StatefulWidget {
+  const _NodeWidget({
+    Key? key,
+    required this.child,
+    required this.node,
+    this.onFocus,
+  }) : super(key: key);
+
+  final Widget child;
+  final GraphNode node;
+  final VoidCallback? onFocus;
+
+  @override
+  _NodeWidgetState createState() => _NodeWidgetState();
+}
+
+class _NodeWidgetState extends State<_NodeWidget> {
+  bool _hovered = false;
+  bool _currentFocus = false;
+
+  @override
+  void initState() {
+    _currentFocus = widget.node.focusNode.hasFocus;
+    widget.node.focusNode.addListener(() {
+      if (_currentFocus != widget.node.focusNode.hasFocus) {
+        if (mounted) {
+          setState(() {
+            _currentFocus = widget.node.focusNode.hasFocus;
+          });
+        }
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var focusColor = Theme.of(context).colorScheme.secondaryVariant;
+
+    return GestureDetector(
+      onTap: () {
+        GraphFocus.of(context).requestFocus(widget.node.focusNode);
+        widget.onFocus?.call();
+      },
+      onPanUpdate: (details) {},
+      child: MouseRegion(
+        onEnter: (event) {
+          setState(() {
+            _hovered = true;
+          });
+        },
+        onExit: (event) {
+          setState(() {
+            _hovered = false;
+          });
+        },
+        child: Container(
+          padding: const EdgeInsets.all(2),
+          decoration: BoxDecoration(
+            border: Border.all(
+                color: (_currentFocus)
+                    ? focusColor
+                    : _hovered
+                        ? focusColor.withAlpha(180)
+                        : Colors.transparent,
+                width: 2),
+            borderRadius: BorderRadius.circular(3),
+          ),
+          child: widget.child,
+        ),
+      ),
+    );
   }
 }
